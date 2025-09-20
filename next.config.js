a0e8c17a71beb0b43f71e92966a08bccfd7ad5d8
ToastFreak <gmcdonald604@gmail.com>
2025-09-20 18:01:04 -0400

    Handle Stripe checkout cancel route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/canceled",
+        destination: "/",
+        permanent: false, // 307 redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
